14cb30ae729d075d87053333232e2a330cc7ea8d
naitto <naitto.digital@gmail.com>
2017-04-20 21:57:59 -0500

    Clase 4
    Colisión de proyectil con enemigo y de Player con enemigo

---
+++ Clase 06.04.17/Michelle Rolando/Assets/Scripts/capsulaenemigo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class capsulaenemigo : MonoBehaviour {
+
+
+    public GameObject explosion;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    //el "other" representa el objeto con el cual el trigger colisiona
+    {
+        if (other.CompareTag("Player"))
+        {
+            //destruimos el objeto que toca el trigger
+            Destroy(other.gameObject);
+            //el objeto se autodestruye
+            Destroy(gameObject);
+            //other.transform.position hace que la animación aparezca desde el enemigo
+            Instantiate(explosion, other.transform.position, transform.rotation);
+
+        }
+
+    }
+}
