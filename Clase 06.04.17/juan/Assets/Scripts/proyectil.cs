41c144a6ef691a328c58d154ae56128c9380e3fa
juan7515236 <juan_7515236@hotmail.com>
2017-04-25 22:05:58 -0500

    cambios

---
+++ Clase 06.04.17/juan/Assets/Scripts/proyectil.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class proyectil : MonoBehaviour {
+    public float speed = 2.0f;
+    public GameObject destroi;
+    public string targetTag;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(targetTag))
+        {
+
+            Destroy(other.gameObject);
+
+            Destroy(gameObject);
+
+            
+        }
+    }
+}
