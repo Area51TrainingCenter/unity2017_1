42c4d248be074da36f34b5bfcb2747c7e4091a60
JoseCusimayta <josecusimaytar@gmail.com>
2017-04-27 11:56:27 -0500

    EnemigAI + Disparo enemigo
    El enemigo ahora se mueve y dispara automaticamente

--- Clase 06.04.17/Jose Cusimayta/Assets/Scripts/disparar.cs
+++ Clase 06.04.17/Jose Cusimayta/Assets/Scripts/disparar.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class disparar : MonoBehaviour {
+    //esta variable nos sirve para recibir el prefab
+    //del proyectil
     public GameObject _prefab;
 	// Use this for initialization
 	void Start () {
@@ -18,6 +20,7 @@ public class disparar : MonoBehaviour {
     {
         if (Input.GetMouseButtonDown(0))
         {
+            //Instantiate crea un clon del prefab que le damos
             Instantiate(_prefab, transform.position, Quaternion.identity);            
         }
     }
