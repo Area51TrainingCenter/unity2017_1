7e2d0f4bad5ba883db9776b12fe2435bde73fc31
paolo1891 <j18.14p.91m@gmail.com>
2017-04-27 22:13:01 -0500

    Clase 6 : Patrones de disparo y colisiones avanzadas

---
+++ Clase 06.04.17/Paolo Marquez/Assets/Scripts/Enemigo/EnemigoAI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoAI : MonoBehaviour {
+    
+    public GameObject _prefab;
+    //esta variable nos da acceso al componente Transform del objeto vacio.
+    //public Transform _spawn;
+    public int sizeBalas=4;
+    public Transform[] _spawns;
+    public float frecuenciaDisparo = 2;
+    
+    // Use this for initialization
+    void Start () {
+        //positionY = transform.position.y*Time.deltaTime;
+        //permite invocar un objeto desde un metodo, desde un tiempo n en segundos, en un intervalo de n segundos
+        //transform.Translate(0, 0, 0);
+        InvokeRepeating("Disparo",0, frecuenciaDisparo);
+        //_spawns = new Transform[sizeBalas];
+        //for(int i=0;i< _spawns.Length; i++)
+        //{
+        //    GameObject g = new GameObject();
+        //    g.position
+        //    _spawns[0] = new GameObject().transform.position;
+        //}
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        
+        
+
+    }
+
+    void Disparo()
+    {
+        //Quaternion rotacion = Quaternion.Euler(0, 0, 180);
+        //Instantiate(_prefab, transform.position, rotacion);
+        //var rota = _spawn.rotation;
+        for (int i = 0; i < _spawns.Length; i++)
+        {
+            Instantiate(_prefab, _spawns[i].position, _spawns[i].rotation);
+        }
+       
+    }
+
+    
+}
---
+++ Clase 06.04.17/Paolo Marquez/Assets/Scripts/SpikeTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrigger : MonoBehaviour {
+    public Rigidbody _prefab;
+    //public GameObject explo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+            Debug.Log("Entro: " + other.name);
+            _prefab.isKinematic = false;
+            //Destroy(other.gameObject);
+            //destruimos el objeto
+            //Destroy(gameObject);
+           // Instantiate(explo, other.transform.position, other.transform.rotation);
+        }
+
+
+    }
+}
