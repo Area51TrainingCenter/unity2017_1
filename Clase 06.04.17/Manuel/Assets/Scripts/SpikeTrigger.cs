9957fcda3e96fe7b724e26c7c15314381dbdd305
naitto <naitto.digital@gmail.com>
2017-04-27 22:15:24 -0500

    mas disparos y detectar zona

---
+++ Clase 06.04.17/Manuel/Assets/Scripts/SpikeTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrigger : MonoBehaviour {
+    public Rigidbody _spike;
+ 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        _spike.isKinematic = false;
+
+    }
+}
