737566f3906cf7412f713c65d3e059acbad2a658
AlexanderLoo <loohu.alexander@gmail.com>
2017-04-27 22:11:46 -0500

    rotacion, listas, ciclo for, etc.

---
+++ Clase 06.04.17/Alexander Loo/Assets/Scripts/SpikeTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrigger : MonoBehaviour {
+
+    public Rigidbody trampa;
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            trampa.isKinematic = false;
+        }
+    }
+}
