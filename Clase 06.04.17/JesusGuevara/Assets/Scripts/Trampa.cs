c56ef2b93de86a978f07f9a852c377303bb3884c
Jesus Guevara <jsus.guevara@gmail.com>
2017-05-03 00:14:43 -0500

    prueba

--- Clase 06.04.17/JesusGuevara/Assets/Scripts/Trampa.cs
+++ Clase 06.04.17/JesusGuevara/Assets/Scripts/Trampa.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Trampa : MonoBehaviour {
-
+    // permite instanciar un Rigibody
     public Rigidbody _preb;
     // Use this for initialization
     void Start () {
