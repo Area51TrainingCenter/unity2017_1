d06f668c2962a0f665507cd65889a5538806493f
xinoHITO <hitoshi_kamisato@hotmail.com>
2017-05-04 22:12:07 -0500

    clase 04.05

--- Clase 06.04.17/Hitoshi Kanno (profesor)/Assets/Scripts/Enemigo/EnemigoAI.cs
+++ Clase 06.04.17/Hitoshi Kanno (profesor)/Assets/Scripts/Enemigo/EnemigoAI.cs
@@ -10,15 +10,47 @@ public class EnemigoAI : MonoBehaviour {
     //un arreglo que contendra elementos de ese tipo
     public Transform[] _spawns;
     public float frecDisparo = 0.5f;
+    public GameObject explosionMuerte;
+
+
+    //creamos una variable global que almacenara la referencia al 
+    //componente Health... de esta forma no tenemos que hacer GetComponent
+    //cada vez que queramos acceder a la vida del enemigo
+    Health healthScript;
+    Renderer _renderer;
+    //esta variable nos da cuanta vida tenia el enemigo en el frame anterior
+    float previousHealth;
 	// Use this for initialization
 	void Start () {
         InvokeRepeating("Disparo", 0, frecDisparo);
+        //asignamos la referencia del componente aquí
+        healthScript = GetComponent<Health>();
+        _renderer = GetComponent<Renderer>();
+        previousHealth = healthScript.health;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        float playerHealth = healthScript.health;
+        if (playerHealth <= 0)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionMuerte, transform.position, transform.rotation);
+        }
+        //preguntamos si la vida en el frame anterior es diferente a la vida actual
+        if (previousHealth != healthScript.health)
+        {
+            _renderer.material.color = Color.white;
+            Invoke("RestaurarColor", 0.1f);
+        }
+        //actualizamos el valor de la variable a la vida actual
+        previousHealth = healthScript.health;
 
-	}
+    }
+
+    void RestaurarColor() {
+        _renderer.material.color = Color.black;
+    }
 
     void Disparo() {
         
