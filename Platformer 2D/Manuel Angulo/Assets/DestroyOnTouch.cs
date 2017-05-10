d73e69103c03bb820ab4451f0cf229b7fe8f94d1
paolo1891 <j18.14p.91m@gmail.com>
2017-05-09 22:03:51 -0500

    q te persigan

---
+++ Platformer 2D/Manuel Angulo/Assets/DestroyOnTouch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnTouch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+	void OnTriggerEnter (Collider other) 
+	{
+		if (other.CompareTag("Player")) 
+		{
+			Destroy (other.gameObject);
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Platformer 2D/Manuel Angulo/Assets/FollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+	public Transform player;
+	// Use this for initialization
+	void Start () 
+	{
+		//buscamos en la escena el GameObject que tenga el tag player
+		//despues obtenemos el componente Transform de ese GameObject
+		//en vez de GetComponent<Transform>() podemos poner solo transform (es como una abreviatura)
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform>();	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//calculamos el vector entre la bal y el player
+		Vector3 direccion = player.position - transform.position;
+		//normalizamos el vector para q  su longitud sea 1
+		direccion.Normalize ();
+		//movemos el objeto usando el vector
+		transform.Translate (direccion * Time.deltaTime);
+	}
+}
