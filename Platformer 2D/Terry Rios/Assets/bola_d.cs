21cc96d5a42323d6e479f59d5863080752facd42
nombre <correo>
2017-05-09 22:03:36 -0500

    clase 9

---
+++ Platformer 2D/Terry Rios/Assets/bola_d.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bola_d : MonoBehaviour {
+	
+
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag ("Player")) 
+		{
+			Destroy(other.gameObject);
+			Destroy (gameObject);
+		
+		
+		
+		}
+	
+	
+	
+	
+	
+	
+	
+	}
+
+
+}
---
+++ Platformer 2D/Terry Rios/Assets/control.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour {
+
+    public float speedX = 5;
+	public float speedY = 5;	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 movevector = new Vector3(0,0,0);
+
+		float h = Input.GetAxis ("Horizontal");
+
+		movevector.x = h * speedX;
+
+
+
+		/*
+		//A
+
+		bool KeyaPressed = Input.GetKey(KeyCode.A);
+
+		if (KeyaPressed)
+		{
+			movevector.x = -speedX;
+		}
+
+		//D
+
+		bool KeydPressed = Input.GetKey(KeyCode.D);
+
+		if (KeydPressed)
+		{
+			movevector.x = speedX;
+		}
+
+		*/
+		transform.Translate(movevector*Time.deltaTime);
+
+
+		
+	}
+
+
+}
