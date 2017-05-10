8c68ff804bf751fbde16be39862fe4a7fcd0083e
paolo1891 <j18.14p.91m@gmail.com>
2017-05-09 21:58:44 -0500

    Clase 9: Movimientos con Axis y aplicacion de vectores
    Se aplico conceptos como normalizacion, magnitud y distancia entre
    vectores.

---
+++ Platformer 2D/Paolo Marquez/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+	public Transform player;
+	public float speed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		//buscamos en la escena el GameObject que tenga el tag player
+		player=GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//calculamos el vector entre la bala y el player
+		Vector3 direccion=player.position-transform.position;
+		direccion.Normalize ();
+		Debug.Log ("direccion:"+direccion);
+		Debug.Log ("Magnitud:"+direccion.magnitude);
+
+		transform.Translate (direccion * Time.deltaTime*speed);
+	}
+}
---
+++ Platformer 2D/Paolo Marquez/Assets/Scripts/PlayerMovimiento.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovimiento : MonoBehaviour {
+	public float speedX = 0.1f;
+	public float speedY = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movimiento ();
+		//Debug.Log ("deltatime=" + Time.deltaTime);
+	}
+
+	void Movimiento()
+	{
+		Vector3 moveVector =new Vector3(0,0,0);
+
+		//movimiento WASD
+		float h=Input.GetAxis("Horizontal");
+		float j=Input.GetAxis("Vertical");
+		//h se multiplica por la velocidad dada
+		moveVector.x = h * speedX;
+		moveVector.y = j * speedY;
+
+//		bool keyWPressed = Input.GetKey(KeyCode.W);
+//		if (keyWPressed)
+//		{
+//			moveVector.y = speedY;
+//		}
+//
+//		bool keyAPressed = Input.GetKey(KeyCode.A);
+//		if (keyAPressed)
+//		{
+//			moveVector.x = -speedX;
+//		}
+//
+//		bool keyDPressed = Input.GetKey(KeyCode.D);
+//		if (keyDPressed)
+//		{
+//			moveVector.x = speedX;
+//		}
+//
+//		bool keySPressed = Input.GetKey(KeyCode.S);
+//		if (keySPressed)
+//		{
+//			moveVector.y = -speedY;
+//
+//		}
+
+
+
+		//al presionar SHift se duplicara la velocidad
+		bool keyLeftShiftPressed = Input.GetKey(KeyCode.LeftShift);
+
+		if (keyLeftShiftPressed)
+		{
+			moveVector.x *= 2;
+			moveVector.y *= 2;
+		}
+
+		//convierte la velocidad por frames a velocidad por segundo
+
+		//transform.Translate(moveX * Time.deltaTime, moveY * Time.deltaTime, 0);
+		transform.Translate(moveVector*Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("Colisione");
+		if(other.CompareTag("enemigo")){
+			Destroy (gameObject);
+			Destroy (other.gameObject);
+		}	
+	}	
+
+}
