bebb61c21695029af98dcb2c04ff706f7493d475
lLviz <luisvicente1397@gmail.com>
2017-05-09 22:00:43 -0500

    Luis
    09.05

---
+++ Platformer 2D/Luis Vicente/Assets/Scripts/Movimiento.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+	public float SpeddX = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 MoveVector = new Vector3 (0,0,0);
+
+		float h = Input.GetAxis ("Horizontal");
+		MoveVector.x = h * SpeddX;
+
+		transform.Translate (MoveVector * Time.deltaTime);
+		//transform.Translate (moveX * Time.deltaTime,0,0);
+	}
+}
---
+++ Platformer 2D/Luis Vicente/Assets/Scripts/Persecusion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Persecusion : MonoBehaviour {
+	public Transform player;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direccion = player.position - transform.position;
+		direccion.Normalize ();
+		transform.Translate (direccion * 5 * Time.deltaTime);
+	}
+}
