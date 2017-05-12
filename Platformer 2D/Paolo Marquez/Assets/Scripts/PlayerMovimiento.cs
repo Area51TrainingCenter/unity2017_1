d84d6a4862a4e54b9b6c3964ff4d0390175542b1
paolo1891 <j18.14p.91m@gmail.com>
2017-05-11 22:14:43 -0500

    Clase 10: aplicaciones fisicas en los juegos

--- Platformer 2D/Paolo Marquez/Assets/Scripts/PlayerMovimiento.cs
+++ Platformer 2D/Paolo Marquez/Assets/Scripts/PlayerMovimiento.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class PlayerMovimiento : MonoBehaviour {
 	public float speedX = 0.1f;
-	public float speedY = 0.1f;
+	public float gravity = -10;
+	private Rigidbody _rigidbody;
+	//los float por defecto valen cero
+	private float verticalSpeed;
+	public float rayLenght=0.6f;
 
 	// Use this for initialization
 	void Start () {
-		
+		_rigidbody = GetComponent<Rigidbody> ();
 	}
 	
 	// Update is called once per frame
@@ -23,51 +27,47 @@ public class PlayerMovimiento : MonoBehaviour {
 
 		//movimiento WASD
 		float h=Input.GetAxis("Horizontal");
-		float j=Input.GetAxis("Vertical");
+		//float j=Input.GetAxis("Vertical");
 		//h se multiplica por la velocidad dada
 		moveVector.x = h * speedX;
-		moveVector.y = j * speedY;
 
-//		bool keyWPressed = Input.GetKey(KeyCode.W);
-//		if (keyWPressed)
-//		{
-//			moveVector.y = speedY;
-//		}
-//
-//		bool keyAPressed = Input.GetKey(KeyCode.A);
-//		if (keyAPressed)
-//		{
-//			moveVector.x = -speedX;
-//		}
-//
-//		bool keyDPressed = Input.GetKey(KeyCode.D);
-//		if (keyDPressed)
-//		{
-//			moveVector.x = speedX;
-//		}
-//
-//		bool keySPressed = Input.GetKey(KeyCode.S);
-//		if (keySPressed)
-//		{
-//			moveVector.y = -speedY;
-//
-//		}
+		Vector3 down = new Vector3 (0, -1, 0);
+		//raycast genera un rayo invisble que te devuelve true si choca al algun suelo o false si no toca nada
+		bool isGrounded=Physics.Raycast (transform.position, down, rayLenght);
+		if(isGrounded==true){
+			//si estoy en el piso el vertical speed es un valor negativo pequeño, para que el jugador toque el piso
+			verticalSpeed = -0.1f;
+		}
+		else{
+			//la gravedad se va aplicando al verticalSpeed
+			verticalSpeed += gravity*Time.deltaTime;
+		}
 
+		moveVector += new Vector3 (0, verticalSpeed, 0);
+
+//		if(isGrounded==false){
+//			//la gravedad se va aplicando
+//			verticalSpeed += gravity*Time.deltaTime;
+//
+//		}	
 
 
 		//al presionar SHift se duplicara la velocidad
-		bool keyLeftShiftPressed = Input.GetKey(KeyCode.LeftShift);
+		bool keyLeftShiftPressed = Input.GetKeyDown(KeyCode.LeftShift);
 
 		if (keyLeftShiftPressed)
 		{
-			moveVector.x *= 2;
-			moveVector.y *= 2;
+//			moveVector.x *= 2;
+//			moveVector.y *= 2;
+			//rayLenght = 2;
 		}
+		//if(keyLeftShiftPressed==false){rayLenght = 0.6f;}
 
 		//convierte la velocidad por frames a velocidad por segundo
 
 		//transform.Translate(moveX * Time.deltaTime, moveY * Time.deltaTime, 0);
-		transform.Translate(moveVector*Time.deltaTime);
+		_rigidbody.velocity=moveVector;
+		//transform.Translate(moveVector*Time.deltaTime);
 	}
 
 	void OnTriggerEnter(Collider other){
