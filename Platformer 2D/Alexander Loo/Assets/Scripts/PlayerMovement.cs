226452febb1240a75f5cd1c39fe224537c1a6fc7
AlexanderLoo <loohu.alexander@gmail.com>
2017-05-16 19:10:10 -0500

    Revert "Revert "Aplicación de la ley de Newton en una plataforma""
    This reverts commit 795104f795ad17642bd6bfc7e0bc7f41ce338046.

--- Platformer 2D/Alexander Loo/Assets/Scripts/PlayerMovement.cs
+++ Platformer 2D/Alexander Loo/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public float speedX = 5;
+	private Rigidbody _rigidbody;
+	private float verticalSpeed;
+	public float gravity = -10;
+	public float rayleght = 1.1f;
 	void Start () {
-		
+		_rigidbody = GetComponent<Rigidbody> ();
 	}
 	void Update () {
 		Movement ();		
@@ -16,6 +20,18 @@ public class PlayerMovement : MonoBehaviour {
 		Vector3 moveVector = new Vector3 (0, 0, 0);
 		float h = Input.GetAxis ("Horizontal");
 		moveVector.x = h * speedX;
+		//Physics.Raycast genera un rayo invisible
+		//que te devuelve true si el rayo toca algo
+		//y false si el rayo no toca nada
+		bool isGrounded = Physics.Raycast (transform.position, new Vector3 (0, -1, 0), rayleght);
+		//la gravedad se va aplicando al verticaSpeed
+		verticalSpeed += gravity * Time.deltaTime;
+		moveVector += new Vector3 (0, verticalSpeed, 0);
+		if (isGrounded) {
+			/*si estoy en el piso el verticalspeed es un valor negativo pequeño...esto es para asegurarnos que el
+			 player toque el piso y no impida el movernos de lado a lado*/
+			verticalSpeed = 0.1f;
+		}
 		//Debug.Log (h);
 		//Debug.Log(moveVector);
 		/*if (Input.GetKey (KeyCode.A)) {
@@ -26,6 +42,7 @@ public class PlayerMovement : MonoBehaviour {
 		}*/
 		//En lugar de pasarle los 3 números por separado le pasamos a la función Translate el Vector3
 		//Cuando multiplicas un número por un Vector3 multiplicas ese número al X,Y y Z por ese número
-		transform.Translate (moveVector * Time.deltaTime);
+		//transform.Translate (moveVector * Time.deltaTime);
+		_rigidbody.velocity = moveVector;
 	}
 }
