b0d365b5aa6440b1db22865657f3e3df66b82037
paolo1891 <j18.14p.91m@gmail.com>
2017-05-18 21:53:37 -0500

    Clase 12: Movimientos a los lados del enemigo

--- Platformer 2D/Paolo Marquez/Assets/Scripts/PlayerMovimiento.cs
+++ Platformer 2D/Paolo Marquez/Assets/Scripts/PlayerMovimiento.cs
@@ -10,9 +10,13 @@ public class PlayerMovimiento : MonoBehaviour {
 	private float verticalSpeed;
 	public float rayLenght=0.6f;
 	public bool isGrounded=false;
+	public bool isTecho=false;
+	public bool isIzq=false;
+	public bool isDer=false;
 	public float salto=10f;
 	public bool jump;
 	public float h;
+	//public bool repulsion=false;
 
 	// Use this for initialization
 	void Start () {
@@ -45,15 +49,43 @@ public class PlayerMovimiento : MonoBehaviour {
 
 		//float j=Input.GetAxis("Vertical");
 		//h se multiplica por la velocidad dada
-		moveVector.x = h * speedX;
+		//if(repulsion==false){
+			moveVector.x = h * speedX;	
+		//}
+		
 
 		Vector3 down = new Vector3 (0, -1, 0);
+		Vector3 up = new Vector3 (0, 1, 0);
+		Vector3 izq = new Vector3 (-1, 0, 0);
+		Vector3 der = new Vector3 (1, 0, 0);
 		//raycast genera un rayo invisble que te devuelve true si choca al algun suelo o false si no toca nada
 		//bool isGrounded=Physics.Raycast (transform.position, down, rayLenght);
 
 		Vector3 boxSize = new Vector3 (transform.localScale.x,transform.localScale.y,transform.localScale.z);
 		boxSize *= 0.99f;
 		isGrounded=Physics.BoxCast (transform.position, boxSize/2, down, Quaternion.identity,rayLenght);
+		isTecho=Physics.BoxCast (transform.position, boxSize/2, up, Quaternion.identity,rayLenght);
+		isIzq=Physics.BoxCast (transform.position, boxSize/2, izq, Quaternion.identity,rayLenght);
+		isDer=Physics.BoxCast (transform.position, boxSize/2, der, Quaternion.identity,rayLenght);
+
+		if((isIzq || isDer) && isGrounded==false){
+
+			if(h!=0){
+				Debug.Log("Choque con pared izq o der");
+				moveVector.x = 0;
+				
+				//moveVector.x = 100;
+				//verticalSpeed = -10;
+				//verticalSpeed += gravity*Time.deltaTime;
+				//moveVector += new Vector3 (100, -verticalSpeed, 0);
+				//repulsion=true;
+			}
+
+		}
+
+		if(isTecho){
+			verticalSpeed = -10;
+		}
 		if(isGrounded==true){
 			//si estoy en el piso el vertical speed es un valor negativo pequeño, para que el jugador toque el piso
 			verticalSpeed = -0.1f;
@@ -62,6 +94,7 @@ public class PlayerMovimiento : MonoBehaviour {
 				verticalSpeed = salto;
 				jump = false;
 			}
+			//repulsion=false;
 		}
 		else{
 			//la gravedad se va aplicando al verticalSpeed
@@ -70,7 +103,7 @@ public class PlayerMovimiento : MonoBehaviour {
 
 		moveVector += new Vector3 (0, verticalSpeed, 0);
 
-
+		Debug.Log("moveVector="+moveVector);
 		_rigidbody.velocity=moveVector;
 		//transform.Translate(moveVector*Time.deltaTime);
 	}
