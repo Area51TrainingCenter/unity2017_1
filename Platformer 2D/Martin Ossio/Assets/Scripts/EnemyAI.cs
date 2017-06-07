5f94225e2842ce8b021699f7517d16f7907b9662
Martin <martinossio@gmail.com>
2017-06-06 21:49:51 -0500

    hola
    hola

---
+++ Platformer 2D/Martin Ossio/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+	public float rayLength = 0.03f;
+	public LayerMask _mask;
+	private bool _goToTheRight;
+	private Rigidbody2D _rigidbody;
+	private Health _healthScript;
+	// Use this for initialization
+	void Start () {
+		_rigidbody = GetComponent<Rigidbody2D> ();
+		_healthScript = GetComponent<Health> ();
+	}
+
+	void Update(){
+		if (_healthScript.health <= 0) {
+			Destroy (gameObject);
+		}
+	}
+
+	void FixedUpdate () {
+		Vector3 boxSize = new Vector3 (transform.localScale.x, transform.localScale.y, transform.localScale.z);
+		boxSize = boxSize * 0.99f;
+		RaycastHit2D hitInfo;
+		hitInfo = Physics2D.BoxCast (transform.position, boxSize, 0, Vector2.up, rayLength,_mask.value);
+
+//		if (hitInfo.collider != null) {
+//			if (hitInfo.collider.gameObject.CompareTag("Player")) {
+//				hitInfo.collider.GetComponent<Health> ().ChangeHealth(20);
+//			}
+//		}
+
+		hitInfo = Physics2D.BoxCast (transform.position, boxSize, 0, Vector2.right, rayLength,_mask.value);
+
+		if (hitInfo.collider != null) {
+			if (hitInfo.collider.gameObject.CompareTag ("Player")) {
+			//	hitInfo.collider.GetComponent<Health> ().ChangeHealth(20);
+			} else {
+				_goToTheRight = !_goToTheRight;
+			}
+		}
+
+		hitInfo = Physics2D.BoxCast (transform.position, boxSize, 0, Vector2.left, rayLength,_mask.value);
+		if (hitInfo.collider !=null) {
+			if (hitInfo.collider.gameObject.CompareTag("Player")) {
+			//	hitInfo.collider.GetComponent<Health> ().ChangeHealth(20);
+			}
+			else {
+				_goToTheRight = !_goToTheRight;
+			}
+		}
+
+		Vector3 moveVector = new Vector3 (-0, 0, 0);
+		if (_goToTheRight) {
+			moveVector *= -1;
+		}
+		_rigidbody.velocity = moveVector;
+
+	}
+}
---
+++ Platformer 2D/Martin Ossio/Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+	public float health = 100;
+	public float maxHealth = 100;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void ChangeHealth (float damage) {
+		health -= damage;
+
+		if (health > maxHealth) {
+			health = maxHealth;
+		}
+
+		if (health < 0) {
+			health = 0;
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
