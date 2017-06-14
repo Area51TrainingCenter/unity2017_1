730492d9eed5a7de7dfaa73e4172005d349189fe
Jesus Guevara <jsus.guevara@gmail.com>
2017-06-13 21:49:28 -0500

    Barra de vida a Zero y Redimencionar los hitboxes
    - ataque 3 del combo a zero

---
+++ Platformer 2D/JesusGuevara/Assets/LifeBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// importamos el codigo de los elementos de UI
+using UnityEngine.UI;
+
+public class LifeBar : MonoBehaviour {
+
+	public GameObject _gameobject;
+	private Image image;
+
+	// Use this for initialization
+	void Start () {
+//		vidaactual / vidamaximo;
+		image = GetComponent<Image>();
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+		float vidaactual =  _gameobject.GetComponent<Health> ().health;
+		float vidamaxima =  _gameobject.GetComponent<Health> ().maxHealth;
+		image.fillAmount = vidaactual/vidamaxima ;
+
+	}
+
+
+}
--- Platformer 2D/JesusGuevara/Assets/PlayerAnimationEvents.cs
+++ Platformer 2D/JesusGuevara/Assets/PlayerAnimationEvents.cs
@@ -52,5 +52,30 @@ public class PlayerAnimationEvents : MonoBehaviour {
 		_gameobject.GetComponent<Movimiento> ().canAttack = false;	
 	}
 
+	// Box RIGHT - Ataque 1
+	public void SetAttack1HitboxSize(){			
+
+		_boxright.GetComponent<BoxCollider2D> ().size = new Vector2 (0.9757428f, 1f);
+		_boxright.GetComponent<BoxCollider2D> ().offset = new Vector2 (-0.01212859f, 0f);	
+
+		_boxLeft.GetComponent<BoxCollider2D> ().size = new Vector2 (0.9747028f, 1f);
+		_boxLeft.GetComponent<BoxCollider2D> ().offset = new Vector2 (-0.01212859f, 0f);
+
+	}
+
+	public void SetAttack2HitboxSize(){
+		
+		_boxright.GetComponent<BoxCollider2D> ().size = new Vector2 (1.421691f, 0.4888293f);
+		_boxright.GetComponent<BoxCollider2D> ().offset = new Vector2 (0.2108457f, 0.08244696f);	
+
+
+		_boxLeft.GetComponent<BoxCollider2D> ().size = new Vector2 (1.466301f, 0.4558502f);
+		_boxLeft.GetComponent<BoxCollider2D> ().offset = new Vector2 (-0.2584446f, 0.05771273f);	
+
+	}
+
+	public void SetAttack3HitboxSize(){
+	}
+
 
 }
