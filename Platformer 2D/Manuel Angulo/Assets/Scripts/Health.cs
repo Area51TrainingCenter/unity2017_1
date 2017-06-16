ac47c0b545421f54d9f247b79417c511cb309837
dgdfg <gdsfgdfg>
2017-06-15 19:09:06 -0500

    clas anterior

---
+++ Platformer 2D/Manuel Angulo/Assets/Scripts/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+	public float health = 100;
+	public float maxHealth = 100;
+	//quien fue el ultimo agresor que te hizo dano
+	public GameObject lastAttacker;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void ChangeHealth (float damage,GameObject attacker) {
+		health -= damage;
+		if (health > maxHealth) {
+			health = maxHealth;
+		}
+		if (health < 0) {
+			health = 0;
+		}
+
+		lastAttacker = attacker;
+	}
+}
