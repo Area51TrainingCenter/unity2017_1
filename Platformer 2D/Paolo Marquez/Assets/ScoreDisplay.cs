ce4220131891b678538e799990d1e257b507aef6
paolo1891 <j18.14p.91m@gmail.com>
2017-06-15 21:09:29 -0500

    clase 17- ajustes de salud

---
+++ Platformer 2D/Paolo Marquez/Assets/ScoreDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+	private Text _text;
+	private ScoreManager _score;
+	// Use this for initialization
+	void Start () {
+		_text = GetComponent<Text> ();
+		_score = GameObject.Find ("ScoreManager").GetComponent<ScoreManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_text.text ="Score: "+ _score.score;
+	}
+}
---
+++ Platformer 2D/Paolo Marquez/Assets/ScoreWhenDeath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreWhenDeath : MonoBehaviour {
+	public int score=100;
+	private ScoreManager _scoreManager;
+	// Use this for initialization
+	void Start () {
+		_scoreManager=GameObject.Find ("ScoreManager").GetComponent<ScoreManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GetComponent<Health>().healht==0) {
+			_scoreManager.score += score;
+		}
+	}
+}
