62de451240a992399a5ac0933012e0f71298f34a
paolo1891 <j18.14p.91m@gmail.com>
2017-06-22 21:57:36 -0500

    Clase 19: Animaciones de pausa y quit

--- Platformer 2D/Paolo Marquez/Assets/LoadScore.cs
+++ Platformer 2D/Paolo Marquez/Assets/LoadScore.cs
@@ -5,13 +5,13 @@ using UnityEngine.UI;
 
 public class LoadScore : MonoBehaviour {
 	private Text _text;
-	private ScoreManager _score;
+
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("El score total es:" + _score);
-		_text = GetComponent<Text> ();
-		_score = GameObject.Find ("ScoreManager").GetComponent<ScoreManager> ();
-		int score=PlayerPrefs.GetInt ("playerScore",_score.score);
+		
+		_text = GetComponentInChildren<Text> ();
+
+		int score=PlayerPrefs.GetInt ("playerScore",0);
 		_text.text = "Score: " + score;
 	}
 	
@@ -19,4 +19,5 @@ public class LoadScore : MonoBehaviour {
 	void Update () {
 		
 	}
+
 }
---
+++ Platformer 2D/Paolo Marquez/Assets/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+	private bool inPause;
+	public RectTransform _panel;
+	// Use this for initialization
+	void Start () {
+		_panel.localPosition = Vector3.one*999;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			inPause = !inPause;
+
+			//si esta en pausa, el juego se congela
+			if (inPause) {
+				Time.timeScale = 0;
+				//local posicion es la posicion del objeto relativo a su padre
+				_panel.localPosition = Vector3.zero;
+			}
+			else  {
+				Time.timeScale = 1;
+				_panel.localPosition = Vector3.one*999;
+			}
+		}
+	}
+
+
+	public void resumeGame () {
+		inPause = false;
+		Time.timeScale = 1;
+		_panel.localPosition = Vector3.one*999;
+	}
+
+	public void quitGame(){
+		Application.Quit ();
+	}
+}
