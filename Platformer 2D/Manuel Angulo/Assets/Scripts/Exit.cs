ebd0608e3e1167472995a52bad5c017caefe8e1c
dgdfg <gdsfgdfg>
2017-06-22 19:31:01 -0500

    22/06 avance

--- Platformer 2D/Manuel Angulo/Assets/Scripts/Exit.cs
+++ Platformer 2D/Manuel Angulo/Assets/Scripts/Exit.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class Exit : MonoBehaviour {
 	private bool _exit = false;
 	public GameObject Player;
+	private bool touchedTrigger = false;
 
 	// Use this for initialization
 	void Start () {
@@ -13,24 +14,35 @@ public class Exit : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	void Update () {
-		if (_exit) {
-			Player.transform.Translate (0, 10 * Time.deltaTime, 0);	
-		}
 
-	}
 
 	void OnTriggerEnter2D(Collider2D other) {
 		
 		if (other.CompareTag("Player")) {
-			other.GetComponentInChildren<Animator>().SetTrigger("exit");
-			other.GetComponent<PlayerMovement> ().enabled = false;
+			//hacemos esto para q el player ya no se pueda mover
+			//en horizontal y para permitir que caiga al piso
+			Player.GetComponent<PlayerMovement> ().canControl = false;
+			touchedTrigger = true;
+		}
+	
+	}
+
+	void Update () {
+
+		if (Player.GetComponent<PlayerMovement>().isGrounded) {
+			Player.GetComponentInChildren<Animator>().SetTrigger("exit");
+			Player.GetComponent<PlayerMovement> ().enabled = false;
 			//new Vector2 (0, 0) = VEctor2.zero
-			other.GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+			Player.GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
 			Invoke ("Salidaaa", 0.9f); 
 			Invoke ("changeScene", 1.5f);
 		}
-	
+
+
+		if (_exit) {
+			Player.transform.Translate (0, 10 * Time.deltaTime, 0);	
+		}
+
 	}
 	void Salidaaa (){
 		_exit = true;
