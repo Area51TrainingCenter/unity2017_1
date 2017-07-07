d4f22a1088b0b529835f21d8a9ae4aeba1cae73a
José Cusimayta <josecusimaytar}@gmail.com>
2017-07-06 21:55:18 -0500

    Plataform 3D
    Movimiento
    -- Movimiento hacia las 4 direcciones
    -- Salto
    --Correr
    ----------------------------------------------------
    Fisicas con Character Controller
    ---------------------------------------------------
    Personaje 3D agregado
    -- Animaciones con nuevas opciones para las transiciones
    ------Usando Lerp para manipular gradualmente las variables del animator

---
+++ Platformer 3D/Jose Cusimayta/Assets/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+	public Transform Player;
+	public Vector3 distancia;
+	// Use this for initialization
+	void Start () {
+		distancia = transform.position - Player.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Player) {
+			transform.position = Player.position + distancia;
+		}
+	}
+}
