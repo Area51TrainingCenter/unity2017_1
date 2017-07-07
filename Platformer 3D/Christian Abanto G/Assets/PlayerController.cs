98d32a3897332a557e2f4893f05458aa91ed69a5
christian <naitto.digital@gmail.com>
2017-07-06 22:02:41 -0500

    M3 :: Clase 2 - Implementacion de Animaciones ( Usando Blend Tree  )
    - Se integro animacion Idle
    - Se integro animacion Walk

--- Platformer 3D/Christian Abanto G/Assets/PlayerController.cs
+++ Platformer 3D/Christian Abanto G/Assets/PlayerController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	public float speed = 5f;
+	public float runSpeed = 10f;
 	private CharacterController _controller;
-	private float gravityVector;
+	private float verticalSpeed;
 	public float gravity;
 	public float jump = 10;
+
+	// variables animator
+	private Animator _animator;
 
 	// Use this for initialization
 	void Start () {
 		_controller = GetComponent<CharacterController> ();
+		_animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -24,16 +29,16 @@ public class PlayerController : MonoBehaviour {
 			//  definir vector movimiento
 				Vector3 moveVector = new Vector3 (ejeH, 0, ejeV);
 			// 	definir vector gravedad
-				if (_controller.isGrounded) 
-				{ gravityVector = -0.1f; }
+				if (!_controller.isGrounded) 
+				{ verticalSpeed -= gravity * Time.deltaTime; }
 				else
-				{ gravityVector -= gravity * Time.deltaTime; }
-				Vector3 verticalVector = new Vector3 (0, gravityVector , 0);
+				{ saltar (); }
+				Vector3 gravityVector = new Vector3 (0, verticalSpeed , 0);
 			//  normalizar movimiento ( para que siempre sea 1 la direccion )
 				moveVector.Normalize (); 
 			//  actualizar vector ( usando la velocidad y gravedad )
-				moveVector *= speed;
-				moveVector += verticalVector;
+				moveVector *= Input.GetKey (KeyCode.LeftShift) ? runSpeed : speed; // determinamos velocidad es aumentado o normal
+				moveVector += gravityVector;
 				moveVector *= Time.deltaTime;
 			//  aplicar direccion de movimiento
 				//transform.Translate(moveVector, Space.World);
@@ -41,11 +46,55 @@ public class PlayerController : MonoBehaviour {
 			//  reestablecer Y				
 				moveVector.y = 0; // ( gravedad no debe aplicarle )
 				float ejeY = 0;
-				if (Input.GetKeyDown (KeyCode.Space)) {
-					moveVector.y = jump;
-				}
+
 		//  ROTAR
 			// rotara en base a su direccion de movimiento ( moveVector )
 			transform.LookAt( transform.position + moveVector );
+
+		//  ANIMATOR 
+			// ¿esta caminando?
+				// old ( utilizando animacion independiente )
+					//bool isWalk = ejeH != 0 || ejeV != 0 ? true : false;
+					//_animator.SetBool ("isWalk", isWalk);
+				// new ( utilizando Blend Tree, que es un agrupador de animaciones )
+					// definimos speed final
+					float end;
+					if (ejeH != 0 || ejeV != 0) { 
+						end = 1; 
+						// verificar si esta corriendo
+						if ( Input.GetKey (KeyCode.LeftShift) ) {
+							end = 2;
+						} 
+					} else { 
+						end = 0; 
+					}
+					// obtenemos speed inicial
+					float start = _animator.GetFloat ("speed");
+					// calculamos resultado
+					float result = Mathf.Lerp (start, end, Time.deltaTime * 5);
+					// seteamos valor speed
+					_animator.SetFloat ("speed", result); 
+						// Nota: 
+						// en el animator, el Blend Tree con nombre "Movement", 
+						// su campo "Threshold" son los valores definidos por nosotros,
+						// si al setear el valor de "speed" concuerda con uno de los valores de "Threshold" 			
+						// entonces cambiara la animación.
 	}
+
+	void saltar(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			verticalSpeed = jump;
+		} else {
+			verticalSpeed = -0.1f;
+		}
+	}
+
+	void FixedUpdate(){
+		
+	}
+
+	///////////////////////// RECICLAJE
+	// ¿esta corriendo?
+	//bool isRun =  Input.GetKey (KeyCode.LeftShift);
+	//_animator.SetBool ("isRun", isRun);
 }
