33f458cc6385ff88a2071d8857d63e8c12366a22
lLviz <luisvicente1397@gmail.com>
2017-07-06 19:05:38 -0500

    LuisVicente
    3D

---
+++ Platformer 3D/LuisVicente3D/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+	public float Speed = 5;
+	public float verticalSpeed = 0;
+	private CharacterController _controler;
+	public float gravity;
+
+	// Use this for initialization
+	void Start () {
+		_controler = GetComponent <CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			// Hacia los Costados
+		float EjeX = Input.GetAxis("Horizontal") ;
+			// Hacia Adelante y Atras
+		float EjeZ = Input.GetAxis("Vertical") ;
+		if (!_controler.isGrounded) {
+			verticalSpeed -= 1;
+		} else {
+			Saltar ();
+		}
+
+		Vector3 Gravedad = new Vector3 (0,verticalSpeed,0);
+		Vector3 moveVector = new Vector3 (EjeX,0,EjeZ);
+		moveVector.Normalize ();
+		/*if (_controler.isGrounded) {
+			verticalSpeed = 0.1f;
+		} else {
+			verticalSpeed -= gravity * Time.deltaTime;
+		}*/
+		moveVector += Gravedad;
+		moveVector *= Speed ; 
+		moveVector *= Time.deltaTime;
+		//transform.Translate (moveVector,Space.World);
+		_controler.Move (moveVector );
+		moveVector.y = 0;
+		transform.LookAt (transform.position + moveVector);
+
+	}
+	void Saltar(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			verticalSpeed = 7f;
+		}
+	}
+}
