af13641f624f30c2be2fe056c1b25af06caf9a83
lLviz <luisvicente1397@gmail.com>
2017-07-06 21:59:36 -0500

    luisVicente
    Ethan

--- Platformer 3D/LuisVicente3D/Assets/Scripts/PlayerControler.cs
+++ Platformer 3D/LuisVicente3D/Assets/Scripts/PlayerControler.cs
@@ -6,21 +6,28 @@ public class PlayerControler : MonoBehaviour {
 	public float Speed = 5;
 	public float verticalSpeed = 0;
 	private CharacterController _controler;
-	public float gravity;
-
+	public float gravity = 0;
+	public float Correr = 0;
+	public Animator _animation;
+	private float EjeX;
+	private float EjeZ;
 	// Use this for initialization
 	void Start () {
 		_controler = GetComponent <CharacterController> ();
+		_animation = GetComponent <Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 			// Hacia los Costados
-		float EjeX = Input.GetAxis("Horizontal") ;
+
+		EjeX = Input.GetAxis("Horizontal") ;
 			// Hacia Adelante y Atras
-		float EjeZ = Input.GetAxis("Vertical") ;
+		EjeZ = Input.GetAxis("Vertical") ;
+
+
 		if (!_controler.isGrounded) {
-			verticalSpeed -= 1;
+			verticalSpeed -= gravity * Time.deltaTime;
 		} else {
 			Saltar ();
 		}
@@ -28,23 +35,43 @@ public class PlayerControler : MonoBehaviour {
 		Vector3 Gravedad = new Vector3 (0,verticalSpeed,0);
 		Vector3 moveVector = new Vector3 (EjeX,0,EjeZ);
 		moveVector.Normalize ();
-		/*if (_controler.isGrounded) {
-			verticalSpeed = 0.1f;
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			moveVector *= Correr;
 		} else {
-			verticalSpeed -= gravity * Time.deltaTime;
-		}*/
+			moveVector *= Speed ;
+		}
+
 		moveVector += Gravedad;
-		moveVector *= Speed ; 
 		moveVector *= Time.deltaTime;
 		//transform.Translate (moveVector,Space.World);
 		_controler.Move (moveVector );
 		moveVector.y = 0;
 		transform.LookAt (transform.position + moveVector);
-
+		Animaciones ();
 	}
+
+
 	void Saltar(){
-		if (Input.GetKeyDown (KeyCode.Space)) {
+		if (Input.GetKey(KeyCode.Space)) {
 			verticalSpeed = 7f;
 		}
+	}
+	void Animaciones (){
+		float end;
+
+		if (EjeX != 0 || EjeZ != 0) {
+			if (Input.GetKey (KeyCode.LeftShift)) {
+				end = 2;
+			}else {
+			end = 1;
+			}
+		} else {
+			end = 0;
+		}	
+
+		float star = _animation.GetFloat ("Speed");
+		float result = Mathf.Lerp (star, end, Time.deltaTime * 5);
+		_animation.SetFloat ("Speed", result);
+	
 	}
 }
