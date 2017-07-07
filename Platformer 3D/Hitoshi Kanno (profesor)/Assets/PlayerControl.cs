5e1e463ccffa805c34e0f23ee70b4c41f39b07f4
xinoHITO <hitoshi_kamisato@hotmail.com>
2017-07-06 22:08:23 -0500

    Clase 02 - modulo 4 programacion
    -codigo para correr
    -importar modelo 3d de ethan y animaciones
    -aplicar animaciones de idle,caminar y correr
    -Uso de blend trees para transiciones fluidas entre idle,caminar y
    correr.

--- Platformer 3D/Hitoshi Kanno (profesor)/Assets/PlayerControl.cs
+++ Platformer 3D/Hitoshi Kanno (profesor)/Assets/PlayerControl.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
 	public float speed = 5;
+	public float runSpeed = 8;
 	private float verticalSpeed = 0;
 	private CharacterController _controller;
+	private Animator _animator;
 	public float gravity = 10;
 	public float jumpForce = 20;
 	// Use this for initialization
 	void Start () {
 		_controller = GetComponent<CharacterController> ();	
+		_animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -20,7 +23,12 @@ public class PlayerControl : MonoBehaviour {
 
 		Vector3 moveVector = new Vector3 (h, 0, v);
 		moveVector.Normalize ();
-		moveVector *= speed;
+
+		if (Input.GetKey(KeyCode.LeftShift)) {
+			moveVector *= runSpeed;
+		}else{
+			moveVector *= speed;
+		}
 
 		if (_controller.isGrounded) {
 			verticalSpeed = -0.1f;
@@ -39,5 +47,30 @@ public class PlayerControl : MonoBehaviour {
 		_controller.Move (moveVector);
 		moveVector.y = 0;
 		transform.LookAt (transform.position + moveVector);
+
+		//lerp requiere 3 valores
+		//tu valor inicial
+		//el valor de destino
+		//velocidad con la que quieres llegar al destino
+
+		//el valor de destino varía segun estemos moviendonos o no
+		float end;
+		if (h != 0 || v != 0) {
+			if (Input.GetKey (KeyCode.LeftShift)) {
+				end = 2;
+			} else {
+				end = 1;
+			}
+		}else{
+			end = 0;
+		}
+		//el valor inicial es el valor actual del parametro speed
+		float start = _animator.GetFloat ("speed");
+		//la funcion Lerp SOLAMENTE CALCULA EL NUMERO no hace nada con el animator
+		//por lo que almacenamos el resultado de lerp en una variable
+		float result = Mathf.Lerp (start, end, Time.deltaTime * 5);
+		//y luego lo aplicamos al parametro speed del animator
+		_animator.SetFloat ("speed", result);
+
 	}
 }
