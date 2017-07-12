7138aba0d61f2a5e93fca3cef623c54b2deffb68
Jesus Guevara <jsus.guevara@gmail.com>
2017-07-11 21:33:18 -0500

    Movimiento Saltar y Agarcharse
    - Input.GetButton(para cambiar la configuracion del teclado)

--- Platformer 3D/JesusGuevarPinedo/Assets/PlayerControl.cs
+++ Platformer 3D/JesusGuevarPinedo/Assets/PlayerControl.cs
@@ -3,83 +3,113 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
-	
-	public float speed = 1;
+	public float speed = 5;
+	public float runSpeed = 8;
 	private float verticalSpeed = 0;
 	private CharacterController _controller;
+	private Animator _animator;
 	public float gravity = 10;
 	public float jumpForce = 20;
-	private float speedRun = 2;
-	private bool conditionRun = false;
-	private Animator _animator;
+
+	public float crouchSpeed = 1;
+
+
 	// Use this for initialization
 	void Start () {
 		_controller = GetComponent<CharacterController> ();	
 		_animator = GetComponent<Animator> ();
 	}
 	
-
+	// Update is called once per frame
 	void Update () {
-		
 		float v = Input.GetAxis ("Vertical");
 		float h = Input.GetAxis ("Horizontal");
 
 		Vector3 moveVector = new Vector3 (h, 0, v);
-
 		moveVector.Normalize ();
-		moveVector *= speed;
+
+
+		if (Input.GetButton("Agacharse")) {
+			moveVector *= crouchSpeed; 
+		} else {
+			
+			// Tecla shift	
+			if (Input.GetButton("Correr")) {
+				moveVector *= runSpeed;
+			}else{
+				moveVector *= speed;
+			}
+
+		}
+
+
+		// Tecla control para Agacharse
+		//if (Input.GetKey (KeyCode.LeftControl)) {
+
+		if(Input.GetButton("Agacharse")){	
+			_animator.SetBool ("isGrouch", true);
+		} else {
+			_animator.SetBool ("isGrouch", false);
+		}
+			
 
 		if (_controller.isGrounded) {
 
 			verticalSpeed = -0.1f;
 			if (Input.GetKeyDown(KeyCode.Space)) {
-				
 				verticalSpeed = jumpForce;
 			}
-
 		}else{
+			
 			verticalSpeed -= gravity*Time.deltaTime;
 		}
 
-		// SHIFT aumentar velocidad
-		conditionRun = false;
-		if (Input.GetKey (KeyCode.LeftShift)) {
-			conditionRun = true;
-			moveVector *= speedRun;		
-		} 
+
 
 		Vector3 gravityVector = new Vector3 (0, verticalSpeed, 0); 
-
 		moveVector += gravityVector;
 		moveVector *= Time.deltaTime;
 		//transform.Translate (moveVector,Space.World);
 		_controller.Move (moveVector);
 		moveVector.y = 0;
-	
-		// ++++++ Animacion Caminar ++++++
-		// El valor de destino varia segun estemos movimiendonos o no
+		transform.LookAt (transform.position + moveVector);
+
+		//lerp requiere 3 valores
+		//tu valor inicial
+		//el valor de destino
+		//velocidad con la que quieres llegar al destino
+
+		//el valor de destino varía segun estemos moviendonos o no
 		float end;
 		if (h != 0 || v != 0) {
-			_animator.SetFloat ("speed", 1);
-			end = 1;		
-		} else {
+			if (Input.GetKey (KeyCode.LeftShift)) {
+				end = 2;
+			} else {
+				end = 1;
+			}
+		}else{
 			end = 0;
-			_animator.SetFloat ("speed", 0);
 		}
 
-		// Correr
-		if (conditionRun) {
-			_animator.SetFloat ("speed", 3);
-		}
-		// el valor inicial es el valor actual del parametro speed
+		//el valor inicial es el valor actual del parametro speed
 		float start = _animator.GetFloat ("speed");
-		// la funcion LERP SOLAMENTE CALCULA el NUMERO no hace nada con el animator
-		// por lo que almacenamos el resultado de LERP en una variable
-		float result = Mathf.Lerp (start, end, Time.deltaTime * 5);
-		// y luego 
-		_animator.SetFloat ("speed", result); 
-		// ++++++++++++++++++++++++
 
-		transform.LookAt (transform.position + moveVector);
+
+		//la funcion Lerp SOLAMENTE CALCULA EL NUMERO no hace nada con el animator
+		//por lo que almacenamos el resultado de lerp en una variable
+		float result = Mathf.Lerp (start, end, Time.deltaTime * 5);
+
+		//y luego lo aplicamos al parametro speed del animator
+		_animator.SetFloat ("speed", result);
+
+
+		// ---- animacion para saltar --
+		if(!_controller.isGrounded){
+		_animator.SetFloat ("verticalSpeed", verticalSpeed);
+		}
+		_animator.SetBool ("isGrounded", _controller.isGrounded);
+	
+	
+
 	}
 }
