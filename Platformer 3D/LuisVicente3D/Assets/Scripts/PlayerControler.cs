502b91ae4d6be46fb2204e6264b4c3f109c5d8fc
lLviz <luisvicente1397@gmail.com>
2017-07-11 21:30:41 -0500

    luisVicente
    Agacharse

--- Platformer 3D/LuisVicente3D/Assets/Scripts/PlayerControler.cs
+++ Platformer 3D/LuisVicente3D/Assets/Scripts/PlayerControler.cs
@@ -8,9 +8,11 @@ public class PlayerControler : MonoBehaviour {
 	private CharacterController _controler;
 	public float gravity = 0;
 	public float Correr = 0;
+	public float CrouchSpeed = 1;
 	public Animator _animation;
 	private float EjeX;
 	private float EjeZ;
+	public float JumpForce;
 	// Use this for initialization
 	void Start () {
 		_controler = GetComponent <CharacterController> ();
@@ -35,10 +37,14 @@ public class PlayerControler : MonoBehaviour {
 		Vector3 Gravedad = new Vector3 (0,verticalSpeed,0);
 		Vector3 moveVector = new Vector3 (EjeX,0,EjeZ);
 		moveVector.Normalize ();
-		if (Input.GetKey (KeyCode.LeftShift)) {
-			moveVector *= Correr;
+		if (Input.GetButton("Agachar")) {
+			moveVector *= CrouchSpeed; 
 		} else {
-			moveVector *= Speed ;
+			if (Input.GetButton("Correr")) {
+				moveVector *= Correr;
+			} else {
+				moveVector *= Speed ;
+			}
 		}
 
 		moveVector += Gravedad;
@@ -52,15 +58,15 @@ public class PlayerControler : MonoBehaviour {
 
 
 	void Saltar(){
-		if (Input.GetKey(KeyCode.Space)) {
-			verticalSpeed = 7f;
+		if (Input.GetButtonDown("Jump")) {
+			verticalSpeed = JumpForce;
 		}
 	}
 	void Animaciones (){
 		float end;
-
+		float endS;
 		if (EjeX != 0 || EjeZ != 0) {
-			if (Input.GetKey (KeyCode.LeftShift)) {
+			if (Input.GetButton("Correr")) {
 				end = 2;
 			}else {
 			end = 1;
@@ -68,10 +74,22 @@ public class PlayerControler : MonoBehaviour {
 		} else {
 			end = 0;
 		}	
-
+	
 		float star = _animation.GetFloat ("Speed");
 		float result = Mathf.Lerp (star, end, Time.deltaTime * 5);
 		_animation.SetFloat ("Speed", result);
-	
+		if (!_controler.isGrounded) {
+			_animation.SetFloat ("VerticalSpeed", verticalSpeed);
+		}
+		_animation.SetBool ("isGrounded", _controler.isGrounded);
+
+		if (Input.GetButton("Agachar")) {
+			_animation.SetBool ("Agachar", true);
+
+		} else {
+			_animation.SetBool ("Agachar", false);
+
+		}
+		
 	}
 }
