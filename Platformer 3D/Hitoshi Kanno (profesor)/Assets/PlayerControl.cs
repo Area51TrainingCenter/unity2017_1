1760d52b5f6becd5cef8368aaa01185754b35b48
xinoHITO <hitoshi_kamisato@hotmail.com>
2017-07-13 21:53:11 -0500

    clase 13.07
    -agacharse debajo de tuneles
    -detectar techos bajos para que no te vuelvas a parar
    -movimiento en base a ejes de la camara y no ejes globales

--- Platformer 3D/Hitoshi Kanno (profesor)/Assets/PlayerControl.cs
+++ Platformer 3D/Hitoshi Kanno (profesor)/Assets/PlayerControl.cs
@@ -6,11 +6,18 @@ public class PlayerControl : MonoBehaviour {
 	public float speed = 5;
 	public float runSpeed = 8;
 	public float crouchSpeed = 1;
+
+	public float gravity = 10;
+	public float jumpForce = 20;
+
+	public LayerMask _mask;
+
+	private Vector3 moveVector;
+	private bool isLowCeiling;
 	private float verticalSpeed = 0;
 	private CharacterController _controller;
 	private Animator _animator;
-	public float gravity = 10;
-	public float jumpForce = 20;
+
 	// Use this for initialization
 	void Start () {
 		_controller = GetComponent<CharacterController> ();	
@@ -22,20 +29,80 @@ public class PlayerControl : MonoBehaviour {
 		float v = Input.GetAxis ("Vertical");
 		float h = Input.GetAxis ("Horizontal");
 
-		Vector3 moveVector = new Vector3 (h, 0, v);
+		GroundMovement (h, v);
+
+		VerticalMovement ();
+
+		moveVector *= Time.deltaTime;
+		_controller.Move (moveVector);
+		moveVector.y = 0;
+		transform.LookAt (transform.position + moveVector);
+
+		Crouch ();
+
+		SetAnimatorParameters (h, v);
+	}
+
+	void FixedUpdate(){
+		bool isCrouched = _animator.GetBool ("crouch");
+		if (isCrouched) {
+			//Physics.Raycast hace el chequeo del raycast pero no vas a poder
+			//ver el rayo en si
+			if (Physics.Raycast (transform.position, Vector3.up, 4.0f, _mask)) {
+				//Debug.DrawRay dibuja la linea... le damos los mismos parametros que el raycast
+				//al pasarle la direccion lo multiplicamos por la longitud que del rayo
+				Debug.DrawRay (transform.position, Vector3.up * 4.0f, Color.green);
+				isLowCeiling = true;
+			} else {
+				Debug.DrawRay (transform.position, Vector3.up * 4.0f, Color.red);
+				isLowCeiling = false;
+			}
+		}
+	}
+
+	void GroundMovement(float h, float v){
+		/*
+
+		(h,0,v)
+		(h,0,0) + (0,0,v)
+		h*(1,0,0)   +   v*(0,0,1)
+		h*Vector3.right    +   v*Vector3.forward
+
+		h*camera.transform.right    +   v*camera.transform.forward
+
+		*/
+
+		//le quitamos el componente "y" a los vectores de la camara
+		//para que el player no quiera irse hacia arriba o hacia abajo
+		//despues de eso normalizamos el vector para asegurarnos que tenga longitud = 1
+		Vector3 cameraForward = Camera.main.transform.forward;
+		cameraForward.y = 0;
+		cameraForward.Normalize ();
+
+		Vector3 cameraRight = Camera.main.transform.right;
+		cameraRight.y = 0;
+		cameraRight.Normalize ();
+
+
+		//Camera.main te devuelve la camara principal de la escena
+		//ahora el movimiento del personaje esta en funcion a la camara y no a
+		//los ejes globales
+		moveVector = (cameraRight * h) + (cameraForward * v);
 		moveVector.Normalize ();
 
-		if (Input.GetButton("Crouch")) {
+		if (Input.GetButton("Crouch") || isLowCeiling) {
 			moveVector *= crouchSpeed;
 		} else {
-			
+
 			if (Input.GetButton("Run")) {
 				moveVector *= runSpeed;
 			}else{
 				moveVector *= speed;
 			}
 		}
+	}
 
+	void VerticalMovement(){
 		if (_controller.isGrounded) {
 			verticalSpeed = -0.1f;
 			if (Input.GetButtonDown("Jump")) {
@@ -48,12 +115,32 @@ public class PlayerControl : MonoBehaviour {
 
 		Vector3 gravityVector = new Vector3 (0, verticalSpeed, 0); 
 		moveVector += gravityVector;
-		moveVector *= Time.deltaTime;
-		//transform.Translate (moveVector,Space.World);
-		_controller.Move (moveVector);
-		moveVector.y = 0;
-		transform.LookAt (transform.position + moveVector);
 
+	}
+
+	void Crouch(){
+		bool isCrouched = _animator.GetBool ("crouch");
+		//cuando estamos agachados reducimos el tamaño del CharacterController
+		if (isCrouched) {
+			_controller.height = 1;
+			//no podemos modificar el y de la variable center defrente
+			//para eso creamos una copia local
+			Vector3 newCenter = _controller.center;
+			//la modificamos
+			newCenter.y = 0.45f;
+			//y luego la asignamos al center de _controller
+			_controller.center = newCenter;
+		} else {
+			if (!isLowCeiling) {
+				_controller.height = 1.8f;
+				Vector3 newCenter = _controller.center;
+				newCenter.y = 0.85f;
+				_controller.center = newCenter;
+			}
+		}
+	}
+
+	void SetAnimatorParameters(float h, float v){
 		//lerp requiere 3 valores
 		//tu valor inicial
 		//el valor de destino
@@ -84,7 +171,7 @@ public class PlayerControl : MonoBehaviour {
 		}
 
 		_animator.SetBool ("isGrounded", _controller.isGrounded);
-		if (Input.GetButton("Crouch")) {
+		if (Input.GetButton("Crouch") || isLowCeiling) {
 			_animator.SetBool ("crouch", true);
 		} else {
 			_animator.SetBool ("crouch", false);
