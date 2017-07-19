695be702747928cd2ab7b47b0a8ba48758a289c7
TerryRios <terry_7_98@hotmail.com>
2017-07-18 22:03:05 -0500

    camara

---
+++ Platformer 3D/Terry_Rios/Assets/freelookcamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class freelookcamera : MonoBehaviour {
+
+	public Transform _target;
+	public Vector3 _offset;
+	private float x;
+	private float y;
+	public float _distance = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float mouseX = Input.GetAxis("Mouse X");
+		float mouseY = Input.GetAxis("Mouse Y");
+
+		x += mouseX * 6;
+		y += mouseY * 6;
+
+
+		Debug.Log (mouseX);
+
+		Quaternion newRotation = Quaternion.Euler (y,x,0);
+
+		Vector3 behind = newRotation * -Vector3.forward;
+		 
+		transform.position = _target.position+_offset+(behind*_distance);
+
+		transform.LookAt (_target.position + _offset);
+		
+	}
+}
--- Platformer 3D/Terry_Rios/Assets/playercontrol.cs
+++ Platformer 3D/Terry_Rios/Assets/playercontrol.cs
@@ -2,164 +2,180 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playercontrol : MonoBehaviour {
-
+public class PlayerControl : MonoBehaviour {
 	public float speed = 5;
-	public float runspeed = 10;
-	public float crouchspeed = 1;
-	public float gravity =-10;
-	private float oncrouch = 0;
-	private float verticalspeed = 0;
-	private bool islowcelling;
+	public float runSpeed = 8;
+	public float crouchSpeed = 1;
+
+	public float gravity = 10;
+	public float jumpForce = 20;
+
 	public LayerMask _mask;
-	private CharacterController _controler;
+
+	private Vector3 moveVector;
+	private bool isLowCeiling;
+	[System.NonSerialized]
+	public float verticalSpeed = 0;
+	private CharacterController _controller;
 	private Animator _animator;
-	private Vector3 movevector;
+
 	// Use this for initialization
 	void Start () {
-
-		_controler = GetComponent<CharacterController> ();
+		_controller = GetComponent<CharacterController> ();	
 		_animator = GetComponent<Animator> ();
-	
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float h = Input.GetAxis ("Horizontal");
 		float v = Input.GetAxis ("Vertical");
+		float h = Input.GetAxis ("Horizontal");
 
-		GroundMovement(h,v);
+		GroundMovement (h, v);
 
-		VerticalMovement ();	
+		VerticalMovement ();
+
+		moveVector *= Time.deltaTime;
+		_controller.Move (moveVector);
+		moveVector.y = 0;
+		transform.LookAt (transform.position + moveVector);
 
 		Crouch ();
 
-		movevector *= Time.deltaTime;
-
-		//transform.Translate (movevector,Space.World);
-		_controler.Move (movevector);
-		movevector.y = 0;
-		transform.LookAt (transform.position+movevector);
-		SetAnimatorParameters (h,v);
-		
-	}
-
-	void SetAnimatorParameters(float h,float v)
-	{
-		float end;
-		if (h !=0 &&_controler.isGrounded == true || v!=0 &&_controler.isGrounded == true) {
-			end=1;
-			if (Input.GetButton("Run")) {
-				end = 2;
-			}
-		}else{
-			end =0;
-		}
-
-		float start = _animator.GetFloat("speed");
-
-		float result= Mathf.Lerp(start,end,Time.deltaTime*5);
-		_animator.SetFloat("speed",result);
-
-
-		if (Input.GetButton("Crouch")&&_controler.isGrounded == true ||islowcelling&&_controler.isGrounded == true) {
-			oncrouch = 1;
-			if (h !=0|| v!=0 ) {
-				oncrouch = 2;
-			}
-
-		} else {
-			oncrouch = 0;
-		}
-		_animator.SetFloat("oncrouch",oncrouch);
-
-		if (!_controler.isGrounded) {
-			_animator.SetFloat ("verticalspeed",verticalspeed);
-		}
-
-		_animator.SetBool ("isgrounded",_controler.isGrounded);
-
+		SetAnimatorParameters (h, v);
 	}
 
 	void FixedUpdate(){
-		
-		if (Input.GetButton("Crouch")&&_controler.isGrounded == true) {
-
-			if (Physics.Raycast(transform.position,Vector3.up,4.0f,_mask)) {
+		bool isCrouched = _animator.GetBool ("crouch");
+		if (isCrouched) {
+			//Physics.Raycast hace el chequeo del raycast pero no vas a poder
+			//ver el rayo en si
+			if (Physics.Raycast (transform.position, Vector3.up, 4.0f, _mask)) {
+				//Debug.DrawRay dibuja la linea... le damos los mismos parametros que el raycast
+				//al pasarle la direccion lo multiplicamos por la longitud que del rayo
 				Debug.DrawRay (transform.position, Vector3.up * 4.0f, Color.green);
-				islowcelling = true;
-
+				isLowCeiling = true;
+			} else {
+				Debug.DrawRay (transform.position, Vector3.up * 4.0f, Color.red);
+				isLowCeiling = false;
 			}
-			else
-			{
-				Debug.DrawRay(transform.position,Vector3.up*4.0f,Color.red);
-				islowcelling = false;
-			}
-
-		} 
-
+		}
 	}
 
-	void GroundMovement(float h,float v){
+	void GroundMovement(float h, float v){
+		/*
+
+		(h,0,v)
+		(h,0,0) + (0,0,v)
+		h*(1,0,0)   +   v*(0,0,1)
+		h*Vector3.right    +   v*Vector3.forward
+
+		h*camera.transform.right    +   v*camera.transform.forward
+
+		*/
+
+		//le quitamos el componente "y" a los vectores de la camara
+		//para que el player no quiera irse hacia arriba o hacia abajo
+		//despues de eso normalizamos el vector para asegurarnos que tenga longitud = 1
+		Vector3 cameraForward = Camera.main.transform.forward;
+		cameraForward.y = 0;
+		cameraForward.Normalize ();
+
+		Vector3 cameraRight = Camera.main.transform.right;
+		cameraRight.y = 0;
+		cameraRight.Normalize ();
 
 
-		Vector3 cameraforward = Camera.main.transform.forward;
-		cameraforward.y = 0;
-		cameraforward.Normalize ();
-		Vector3 cameraright = Camera.main.transform.right;
-		cameraright.y = 0;
-		cameraright.Normalize ();
-		movevector = (cameraright*h) + (cameraforward*v);
-		movevector.Normalize ();
-		movevector *= speed;
-		if (Input.GetButton("Crouch")||islowcelling) {
+		//Camera.main te devuelve la camara principal de la escena
+		//ahora el movimiento del personaje esta en funcion a la camara y no a
+		//los ejes globales
+		moveVector = (cameraRight * h) + (cameraForward * v);
+		moveVector.Normalize ();
 
-			movevector *= crouchspeed;
+		if (Input.GetButton("Crouch") || isLowCeiling) {
+			moveVector *= crouchSpeed;
 		} else {
 
 			if (Input.GetButton("Run")) {
-				movevector *= runspeed;
-			} else {
-				movevector *= speed;
+				moveVector *= runSpeed;
+			}else{
+				moveVector *= speed;
 			}
-
 		}
-
 	}
+
 	void VerticalMovement(){
-
-		if (_controler.isGrounded == true) {
-			verticalspeed = -0.1f;
-			if (Input.GetButton("Jump")) {
-				verticalspeed = 10;
+		if (_controller.isGrounded) {
+			verticalSpeed = -0.1f;
+			if (Input.GetButtonDown("Jump")) {
+				verticalSpeed = jumpForce;
 			}
-
-		} else {
-
-			verticalspeed -= gravity*Time.deltaTime;
+		}else{
+			verticalSpeed -= gravity*Time.deltaTime;
 		}
-		Vector3 gravedad = new Vector3 (0,verticalspeed,0);
-		movevector += gravedad;
+
+
+		Vector3 gravityVector = new Vector3 (0, verticalSpeed, 0); 
+		moveVector += gravityVector;
 
 	}
 
 	void Crouch(){
+		bool isCrouched = _animator.GetBool ("crouch");
+		//cuando estamos agachados reducimos el tamaño del CharacterController
+		if (isCrouched) {
+			_controller.height = 1;
+			//no podemos modificar el y de la variable center defrente
+			//para eso creamos una copia local
+			Vector3 newCenter = _controller.center;
+			//la modificamos
+			newCenter.y = 0.45f;
+			//y luego la asignamos al center de _controller
+			_controller.center = newCenter;
+		} else {
+			if (!isLowCeiling) {
+				_controller.height = 1.8f;
+				Vector3 newCenter = _controller.center;
+				newCenter.y = 0.85f;
+				_controller.center = newCenter;
+			}
+		}
+	}
+
+	void SetAnimatorParameters(float h, float v){
+		//lerp requiere 3 valores
+		//tu valor inicial
+		//el valor de destino
+		//velocidad con la que quieres llegar al destino
+
+		//el valor de destino varía segun estemos moviendonos o no
+		float end;
+		if (h != 0 || v != 0) {
+			if (Input.GetButton("Run")) {
+				end = 2;
+			} else {
+				end = 1;
+			}
+		}else{
+			end = 0;
+		}
+		//el valor inicial es el valor actual del parametro speed
+		float start = _animator.GetFloat ("speed");
+		//la funcion Lerp SOLAMENTE CALCULA EL NUMERO no hace nada con el animator
+		//por lo que almacenamos el resultado de lerp en una variable
+		float result = Mathf.Lerp (start, end, Time.deltaTime * 5);
+		//y luego lo aplicamos al parametro speed del animator
+		_animator.SetFloat ("speed", result);
 
 
-		if (Input.GetButton("Crouch")&&_controler.isGrounded == true) {
+		if (!_controller.isGrounded) {
+			_animator.SetFloat ("verticalSpeed", verticalSpeed);	
+		}
 
-			_controler.height = 1;
-			Vector3 newcenter = _controler.center;
-			newcenter.y = 0.45f;
-			_controler.center = newcenter;
-
-		} else{
-			_controler.height = 1.8f;
-			Vector3 newcenter = _controler.center;
-			newcenter.y = 0.84f;
-			_controler.center = newcenter;
-
+		_animator.SetBool ("isGrounded", _controller.isGrounded);
+		if (Input.GetButton("Crouch") || isLowCeiling) {
+			_animator.SetBool ("crouch", true);
+		} else {
+			_animator.SetBool ("crouch", false);
 		}
 	}
 }
