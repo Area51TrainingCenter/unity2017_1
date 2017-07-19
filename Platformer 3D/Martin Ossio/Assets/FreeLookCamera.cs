2aa415e0d8ee90b850495281c32a7faebb69bf28
Martin <martinossio@gmail.com>
2017-07-18 22:00:41 -0500

    CatCam

---
+++ Platformer 3D/Martin Ossio/Assets/FreeLookCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeLookCamera : MonoBehaviour {
+
+	public Transform target;
+	public Vector3 CameraOffset;
+	public Vector3 newCameraOffset;
+	public Vector3 newCameraOffsetRotation;
+	public float angle = 0;
+	public float angle2 = 0;
+
+	public float distance = 5;
+
+	// Use this for initialization
+	void Start () {
+		newCameraOffset = transform.position - target.position;
+		newCameraOffset.z = 0;
+		// newCameraOffsetRotation = transform.rotation - target.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float mouseX = Input.GetAxis ("Mouse X");
+		angle += mouseX * 6;
+		float mouseY = Input.GetAxis ("Mouse Y");
+		angle2 += mouseY * 6;
+
+		Quaternion newRotation = Quaternion.Euler (angle2, angle, 0); 
+
+		Vector3 behind = newRotation * -Vector3.forward;
+
+		transform.position = target.position + newCameraOffset + (behind*distance);
+
+		transform.LookAt (target.position+newCameraOffset);
+	}
+}
--- Platformer 3D/Martin Ossio/Assets/JumpPad.cs
+++ Platformer 3D/Martin Ossio/Assets/JumpPad.cs
@@ -3,31 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class JumpPad : MonoBehaviour {
-
-	public GameObject _victim;
+	public float jumpForce = 20;
+	private GameObject _victim;
 
 	// Use this for initialization
 	void Start () {
 		_victim = GameObject.FindGameObjectWithTag ("Player");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-
-	}
-
+		
 	void OnTriggerEnter( Collider whoCrashed ) {
-		Debug.Log ("Ingrese");
 		if (whoCrashed.CompareTag("Player")) {
-			Debug.Log ("Es el Player");
 			whoCrashed.transform.Translate (0, 2, 0);
 			Invoke ("JumpPadGo", 0.01f);
 		}
 	}
 
 	void JumpPadGo() {
-		Debug.Log ("Salte");
-		_victim.GetComponent<PlayerControl> ().verticalSpeed = 40;
+		_victim.GetComponent<PlayerControl> ().verticalSpeed = jumpForce;
 	}
 
 }
