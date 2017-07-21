d53e2740785b6a3b11cbbf2d9f790862b6b19b5f
name2 <name2>
2017-07-20 22:05:13 -0500

    Camera Zoom in/out

--- Platformer 3D/Alexander Loo(alumno)/Assets/FreeLookCamera.cs
+++ Platformer 3D/Alexander Loo(alumno)/Assets/FreeLookCamera.cs
@@ -8,25 +8,68 @@ public class FreeLookCamera : MonoBehaviour {
 	//vector que mide la distancia entre la cámara y el objetivo
 	public Vector3 offset;
 	public float distance;
-	public float angle;
+	private float currentDistance;
+	private float targetDistance;
+	private float angleX, angleY;
 	public float rotationSpeed;
 
 	void Start(){
-
+		targetDistance = distance;
 	}
 
 	void Update(){
 
 		float mouseX = Input.GetAxis ("Mouse X");
-		Debug.Log (mouseX);
-		angle += rotationSpeed * mouseX;
+		float mouseY = Input.GetAxis ("Mouse Y");
+		angleX += rotationSpeed * mouseX;
+		angleY += rotationSpeed * mouseY;
+		/*
+		if (angleY > 70) {
+			angleY = 70;
+		}
+		if (angleY < -50) {
+			angleY = -50;
+		}*/
+		//Mathf.Clamp sirve para restringir límintes usando un mínimo y un máximo
+		angleY = Mathf.Clamp (angleY, -50, 70);
 		//Euler te permite crear un Quaternion en base a los valores x,y,z
-		Quaternion newRotation = Quaternion.Euler (0, angle, 0);
+		Quaternion newRotation = Quaternion.Euler (angleY, angleX, 0);
 		/*Cuando multiplicas unas rotación por un vector lo que estas haciendo es rotar el vector en base
 		 a la rotación...y el vector resultante lo guardamos en la variable behind*/
 		Vector3 behind = newRotation * -Vector3.forward;
-		transform.position = target.position + offset + (behind * distance);
+		currentDistance = Mathf.Lerp (currentDistance, targetDistance,Time.deltaTime * 10);
+		transform.position = target.position + offset + (behind * currentDistance);
 		//para asegurarnos que siempre mire al objetivo
 		transform.LookAt (target.position + offset);
+	}
+
+	void FixedUpdate(){
+
+		bool isCameraBehindObstacle = false;
+		//posición del target, se suma offset para que no apunte a los pies(el origen de los modelos 3d);
+		Vector3 targetPos = target.position + offset;
+		RaycastHit hitInfo;
+		//para calcular el vector entre dos puntos, tomando en cuenta que es destino - origen (dirección)
+		Vector3 direction = transform.position - targetPos;
+		//direction.magnitude obtiene la magnitud de un vector
+		if (Physics.Raycast (targetPos, direction, out hitInfo, direction.magnitude)) {
+			Debug.DrawRay (targetPos, direction, Color.green);
+			isCameraBehindObstacle = true;
+		} else {
+			Debug.DrawRay (targetPos, direction, Color.red);
+		}
+		//si hay un obstaculo, la cámara se acerca
+		if (isCameraBehindObstacle) {
+			targetDistance -= Time.fixedDeltaTime * 5;
+		} else {
+			//creamos un raycast mas grande y sólo si no detectamos nada con este retrocedemos la cámara
+			if(!Physics.Raycast (targetPos, direction, out hitInfo, targetDistance + 1)){
+				targetDistance += Time.fixedDeltaTime * 5;
+			//para que no se aleje hacia el infinito
+			if (targetDistance >= distance) {
+				targetDistance = distance;
+				}
+			}
+		}
 	}
 }
