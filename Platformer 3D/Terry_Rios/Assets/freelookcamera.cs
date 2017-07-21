620f6c7ec11b8f0ebcb400a4294cb5e5750a4d42
TerryRios <terry_7_98@hotmail.com>
2017-07-20 22:08:36 -0500

    mejor camara

--- Platformer 3D/Terry_Rios/Assets/freelookcamera.cs
+++ Platformer 3D/Terry_Rios/Assets/freelookcamera.cs
@@ -8,11 +8,53 @@ public class freelookcamera : MonoBehaviour {
 	public Vector3 _offset;
 	private float x;
 	private float y;
+	private float targetdistance;
+	private float currentdistance;
 	public float _distance = 5;
 
 	// Use this for initialization
 	void Start () {
+
+
+		targetdistance = _distance;
+		currentdistance = _distance;
 		
+	}
+
+	void FixedUpdate(){
+
+		Vector3 targetpos =_target.position + _offset;
+		RaycastHit hitinfo;
+		bool isCameraBehindObstacle = false;
+		Vector3 direction = transform.position - targetpos;
+		if (Physics.Raycast (targetpos, direction, out hitinfo, direction.magnitude)) {
+												
+			Debug.DrawRay (targetpos, direction, Color.green);
+
+		   isCameraBehindObstacle = true;		
+		} 
+		else 
+		{
+			Debug.DrawRay (targetpos, direction, Color.red);
+		}
+
+		if (isCameraBehindObstacle) {
+
+			targetdistance -= Time.fixedDeltaTime*5;			
+		} 
+		else 
+		{
+			if (Physics.Raycast (targetpos, direction, out hitinfo,targetdistance+1)) {
+				
+				targetdistance += Time.fixedDeltaTime * 5;
+
+				if (targetdistance >= _distance) {
+					
+					targetdistance = _distance;
+				}
+				
+			}
+		}				
 	}
 	
 	// Update is called once per frame
@@ -24,14 +66,21 @@ public class freelookcamera : MonoBehaviour {
 		x += mouseX * 6;
 		y += mouseY * 6;
 
+		if (y >= 89) {
+			y = 89;
+			
+		}
+		if (y<= -20) {
+			y = -20;
+		}
 
-		Debug.Log (mouseX);
+		//y = Mathf.Clamp ();
 
 		Quaternion newRotation = Quaternion.Euler (y,x,0);
 
 		Vector3 behind = newRotation * -Vector3.forward;
 		 
-		transform.position = _target.position+_offset+(behind*_distance);
+		transform.position = _target.position+_offset+(behind*currentdistance);
 
 		transform.LookAt (_target.position + _offset);
 		
