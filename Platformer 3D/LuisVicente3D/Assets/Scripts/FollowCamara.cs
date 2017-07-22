342c8214d7dee013ef429cfb332140239d791c83
lLvizxD <luisvicente1397@gmail.com>
2017-07-21 21:12:42 -0500

    LuisVicente
    Mejoras de la camara

--- Platformer 3D/LuisVicente3D/Assets/Scripts/FollowCamara.cs
+++ Platformer 3D/LuisVicente3D/Assets/Scripts/FollowCamara.cs
@@ -6,22 +6,71 @@ public class FollowCamara : MonoBehaviour {
 	public Transform target;
 	public Vector3 offset;
 	public float Distance = 5;
-	public float angle = 0;
-	public float SpeedMouse = 0;
+
+	private float targetDistance;
+	private float currentDistance;
+	private float angleX = 0;
+	private float angleY = 0;
+
+	public float minYAngle = -26;
+	public float maxYAngle = 70;
+	public float sensitivity = 6;
+
+	//public float angle = 0;
+	//public float SpeedMouse = 0;
 	// Use this for initialization
 	void Start () {
-		
+		targetDistance = Distance;
+		currentDistance = Distance;
 	}
-	
+
+	void FixedUpdate(){
+		Vector3 targetPos = target.position + offset;
+		RaycastHit hitInfo;
+		bool isCameraBehindObstacle = false;
+		Vector3 direction = transform.position - targetPos;
+		if (Physics.Raycast (targetPos, direction, out hitInfo, direction.magnitude)) {
+			Debug.DrawRay (targetPos, direction, Color.green);
+			isCameraBehindObstacle = true;
+		} else {
+			Debug.DrawRay (targetPos, direction, Color.red);
+		}
+
+		if (isCameraBehindObstacle) {
+			targetDistance -= Time.fixedDeltaTime * 15; 
+		} else {
+			if (!Physics.Raycast (targetPos, direction, out hitInfo, targetDistance+1)) {
+				targetDistance += Time.fixedDeltaTime*15;
+				if (targetDistance > Distance) {
+					targetDistance = Distance;
+				}
+			}
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
 		float mouseX = Input.GetAxis ("Mouse X" );
+		float mouseY = Input.GetAxis ("Mouse Y" );
+		angleX += mouseX * sensitivity;
+		angleY += mouseY * sensitivity;
 
-		angle += mouseX * SpeedMouse;
-	
-		Quaternion NewRotation = Quaternion.Euler(0, angle ,0);
+		//angle += mouseX * SpeedMouse;
+
+		if (angleY < minYAngle) {
+				angleY = minYAngle;
+		}
+		if (angleY > maxYAngle) {
+				angleY = maxYAngle;
+		}
+		angleY = Mathf.Clamp (angleY, minYAngle, maxYAngle);
+		Quaternion NewRotation = Quaternion.Euler(angleY, angleX ,0);
 		Vector3 Behind = NewRotation * -Vector3.forward;
-		transform.position = target.position + offset + (Behind * Distance);
-		transform.LookAt (target.position + offset);
+		currentDistance = Mathf.Lerp (currentDistance, targetDistance, Time.deltaTime * 12);
+		transform.position = target.position+offset+(Behind*currentDistance);
+		transform.LookAt (target.position+offset);
+
+		//transform.position = target.position + offset + (Behind * Distance);
+		//transform.LookAt (target.position + offset);
 	}
 }
