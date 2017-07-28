528fb488b072fcae6c91a2b8564cf0abe582fdd8
Jesus Guevara <jsus.guevara@gmail.com>
2017-07-27 21:55:29 -0500

    Animacion espada.

--- Platformer 3D/JesusGuevarPinedo/Assets/EnemyAI.cs
+++ Platformer 3D/JesusGuevarPinedo/Assets/EnemyAI.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour {
+
 	private Health _health;
-	// Use this for initialization
 	void Start () {
 		_health = GetComponent<Health> ();
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
 
 		if (_health.health == 0) {
