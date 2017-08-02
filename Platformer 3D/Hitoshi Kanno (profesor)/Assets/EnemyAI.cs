eb6ba9732c5b1c12218c89428c628122fccef168
xinoHITO <hitoshi_kamisato@hotmail.com>
2017-08-01 22:04:38 -0500

    clase 01.08
    -efecto de particula cuando golpeas a enemigo
    -knockback de enemigo al ser golpeado

--- Platformer 3D/Hitoshi Kanno (profesor)/Assets/EnemyAI.cs
+++ Platformer 3D/Hitoshi Kanno (profesor)/Assets/EnemyAI.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour {
 	private Health _healthScript;
+	private Animator _animator;
+	private float previousHealth;
+
+	private Vector3 _impact;
+
 	// Use this for initialization
 	void Start () {
+		_animator = GetComponent<Animator> ();
 		_healthScript = GetComponent<Health> ();
+		previousHealth = _healthScript.health;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (_healthScript.health <= 0) {
-			Destroy (gameObject);
+
+		ManageKnockback ();
+
+		ManageAnimatorParameters ();
+
+
+		previousHealth = _healthScript.health;
+
+	}
+
+	void ManageKnockback(){
+
+		_impact = Vector3.Lerp (_impact, Vector3.zero, Time.deltaTime * 3);
+		if (_impact.magnitude < 2) {
+			_impact = Vector3.zero;
+		}
+
+		GetComponent<CharacterController> ().Move (_impact*Time.deltaTime);
+	}
+
+	public void AddImpact (Vector3 direction, float force){
+		_impact = direction * force;
+	}
+
+	void ManageAnimatorParameters(){
+		if (_healthScript.health < previousHealth) {
+
+			if (_healthScript.health <= 0) {
+				_animator.SetTrigger ("die");
+				this.enabled = false;
+			} else {
+				_animator.SetTrigger ("hurt");
+			}
 		}
 	}
 }
--- Platformer 3D/Hitoshi Kanno (profesor)/Assets/MeleeDamage.cs
+++ Platformer 3D/Hitoshi Kanno (profesor)/Assets/MeleeDamage.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MeleeDamage : MonoBehaviour {
+	public Transform owner;
 	public float damage = 20;
+	public GameObject hitEffect;
+	public float knockbackForce = 30;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,6 +15,11 @@ public class MeleeDamage : MonoBehaviour {
 	void OnTriggerEnter (Collider other) {
 		if (other.CompareTag("enemy")) {
 			other.GetComponent<Health> ().ChangeHealth (damage);
+			Instantiate (hitEffect, transform.position, Quaternion.identity);
+			Vector3 dir = other.transform.position - owner.transform.position;
+			dir.y = 0;
+			dir.Normalize ();
+			other.GetComponent<EnemyAI> ().AddImpact (dir, knockbackForce);
 		}
 	}
 }
