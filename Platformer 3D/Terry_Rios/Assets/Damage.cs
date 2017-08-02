895b39e56d431290816e60c198a824d29bb07711
TerryRios <terry_7_98@hotmail.com>
2017-08-01 22:03:19 -0500

    clase 8

--- Platformer 3D/Terry_Rios/Assets/Damage.cs
+++ Platformer 3D/Terry_Rios/Assets/Damage.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Damage : MonoBehaviour {
-
+	
+	public Transform owner;
 	public float _damage = 20;
+	public GameObject _particulas;
+
 
 	// Use this for initialization
 	void Start () {
@@ -20,7 +23,11 @@ public class Damage : MonoBehaviour {
 		if (other.CompareTag("Enemy")) {
 
 			other.GetComponent<health> ().ChangeHealth (_damage);
-
+			Instantiate (_particulas, transform.position, transform.rotation);
+			Vector3 dir = other.transform.position - owner.transform.position;
+			dir.y = 0;
+			dir.Normalize();
+			other.GetComponent<enemigo_ai> ().AddImpact (dir, 30);
 		}
 
 
---
+++ Platformer 3D/Terry_Rios/Assets/enemigo_ai.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemigo_ai : MonoBehaviour {
+
+	private health _scripthealth;
+	private Animator _Animations;
+	private float _previoushealth;
+	private Vector3 _impact;
+	private CharacterController _charactercontroler;
+
+	// Use this for initialization
+	void Start () {
+
+		_scripthealth = GetComponent<health> ();
+		_Animations = GetComponent<Animator> ();
+		_charactercontroler = GetComponent<CharacterController> ();
+		_previoushealth = _scripthealth._health;
+
+	}
+
+	// Update is called once per frame
+	void Update(){
+
+		ManageAnimatorParameters ();
+
+		ManageKnockback ();
+
+		_previoushealth = _scripthealth._health;
+
+
+
+	}
+
+	void ManageKnockback(){
+
+		_impact = Vector3.Lerp (_impact, Vector3.zero, Time.deltaTime * 3);
+			if(_impact.magnitude<2){
+				_impact = Vector3.zero;
+			}
+		_charactercontroler.Move (_impact*Time.deltaTime);
+				
+	}
+
+	public void AddImpact(Vector3 direction,float force){
+
+		_impact = direction * force;
+
+	}
+
+	void ManageAnimatorParameters(){
+
+
+		if (_scripthealth._health < _previoushealth) {
+
+			if (_scripthealth._health <= 0) {
+
+				_Animations.SetTrigger ("health");
+				this.enabled = false;
+
+			} else {
+				_Animations.SetTrigger ("hurt");
+
+			}
+
+
+		}
+
+	}
+		
+}
