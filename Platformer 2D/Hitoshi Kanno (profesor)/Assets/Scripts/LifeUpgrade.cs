84822c20a4cf5832e0a6a41b4cd996f64dc2c222
Hitoshi Kanno <hitoshi_kamisato@hotmail.com>
2017-08-24 21:55:54 -0500

    Clase 24.08
    -PlayerPrefs
    -Checkpoints
    -Coleccionables permamentes que deben desaparecer una vez cogidos por el player ejm. corazones en megaman x
    -incremento de vida maxima y cargar esta vida maxima al inicio del juego

---
+++ Platformer 2D/Hitoshi Kanno (profesor)/Assets/Scripts/LifeUpgrade.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeUpgrade : MonoBehaviour {
+	public string name = "corazon1";
+	// Use this for initialization
+	void Start () {
+		int isHeartTaken = PlayerPrefs.GetInt (name, 0);
+		if (isHeartTaken == 1) {
+			Destroy (gameObject);
+		}
+	}
+	
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.CompareTag("Player")) {
+			Health healthScript = other.GetComponent<Health> ();
+			healthScript.maxHealth += 20;
+			//healthScript.maxHealth = healthScript.maxHealth + 20;
+			healthScript.health = healthScript.maxHealth;
+			PlayerPrefs.SetFloat("playerMaxHealth",healthScript.maxHealth);
+			PlayerPrefs.SetInt (name, 1);
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Platformer 2D/Hitoshi Kanno (profesor)/Assets/Scripts/LoadPlayerData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadPlayerData : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		float savedMaxHealth = PlayerPrefs.GetFloat ("playerMaxHealth",0);
+		GameObject playerObject = GameObject.FindGameObjectWithTag ("Player");
+		if (savedMaxHealth != 0) {
+			Health playerHealthScript = playerObject.GetComponent<Health> ();
+			playerHealthScript.maxHealth = savedMaxHealth;
+			playerHealthScript.health = playerHealthScript.maxHealth;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
