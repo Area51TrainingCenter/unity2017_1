7e377c16fa27d91a1cbc3e20d9f874c6a3d110d0
luis vicente <luisvicente1397@gmail.com>
2017-08-29 22:06:49 -0500

    luisVicente
    multiplayer

---
+++ Platformer Multiplayer Online/luisVicente2DMultiplayer/Assets/PlayerControler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class PlayerControler : NetworkBehaviour {
+	public GameObject _Bola;
+	public Transform _transfor;
+	private Rigidbody2D  _rigibody;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isLocalPlayer) {
+			if (Input.GetKeyDown (KeyCode.E)) {
+				CmdShoot ();
+			}
+		}
+	}
+		[Command]
+		void CmdShoot(){
+		GameObject bala = (GameObject)Instantiate (_Bola, _transfor.position, _transfor.rotation);
+			if (transform.localScale.x > 0) {
+				bala.GetComponent<Rigidbody2D> ().velocity = Vector2.right * 5;
+			}if(transform.localScale.x < 0){
+				bala.GetComponent<Rigidbody2D> ().velocity = Vector2.left * 5;
+		}
+		NetworkServer.Spawn (bala);
+	}
+} 
+
