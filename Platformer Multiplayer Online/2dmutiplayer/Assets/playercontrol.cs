417c0fd43c825f5aac6677c9a2c07ab81a173df8
Terry_Rios <terry_7_98@hotmail.com>
2017-08-29 22:03:03 -0500

    clase 15

---
+++ Platformer Multiplayer Online/2dmutiplayer/Assets/playercontrol.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class playercontrol : NetworkBehaviour {
+
+	public GameObject _circle;
+	public Transform _gun;
+
+	// Use this for initialization
+	void Start () {
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (isLocalPlayer) {
+			if (Input.GetButtonDown ("Fire1")) {
+
+				Cmdbullet ();
+		
+			}
+		}
+
+		
+	}
+
+	[Command]
+
+	void Cmdbullet(){
+
+		GameObject newBullet = (GameObject)Instantiate (_circle, _gun.position, Quaternion.identity);
+		if (transform.localScale.x>0) {
+
+			newBullet.GetComponent<Rigidbody2D> ().velocity = new Vector2(10,0);
+
+		}
+		if (transform.localScale.x<0) {
+
+			newBullet.GetComponent<Rigidbody2D> ().velocity = new Vector2(-10,0);
+
+		}
+
+		NetworkServer.Spawn (newBullet);
+
+			
+	}
+}
