198b6ac5ad69dd6f25bc2b4ad3c1b3253427ac14
Terry_Rios <terry_7_98@hotmail.com>
2017-08-31 22:00:57 -0500

    clase 16

---
+++ Platformer Multiplayer Online/2dmutiplayer/Assets/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HealthBar : MonoBehaviour {
+
+	public Health _healthscript;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		GetComponent<Image> ().fillAmount = _healthscript._health / _healthscript._Maxhealth;
+		
+	}
+}
