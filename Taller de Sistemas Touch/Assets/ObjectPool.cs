b3c5209d7400bb7843cf11e3f6036acb099be606
luis vicente <luisvicente1397@gmail.com>
2017-09-05 21:10:07 -0500

    Taller de Object Pooling
    Mantienes presionada la barra espaciadora y el cuadrado dispara balas.
    
    Las balas se encunetran en un pool que se apagan y prenden para reusarse.

---
+++ Taller de Sistemas Touch/Assets/ObjectPool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour {
+	//prefab original de la bala que vamos a clonar
+	public GameObject _prefab;
+	//creamos una lista de las copias de la bala
+	public GameObject[] _instances;
+
+	// Use this for initialization
+	void Start () {
+		//cambiamos el nombre de este gameObject para que sea "Bullet pool"
+		gameObject.name = _prefab.name + " pool";
+		//creamos nuestro arreglo y fijamos la cantidad de elementos que tendrá
+		_instances = new GameObject[25];
+		for (int i = 0; i < 25; i++) {
+			//creamos la bala
+			GameObject newBullet = (GameObject) Instantiate (_prefab);
+			//desactivamos la bala
+			newBullet.SetActive (false);
+			//colocamos la bala dentro del arreglo
+			_instances [i] = newBullet;
+
+		}
+	}
+
+	//esta funcion se encarga de que la bala aparezca
+	public GameObject Spawn(Vector3 pos, Quaternion rot){
+		for (int i = 0; i < 25; i++) {
+			//preguntamos si esta copia de la bala esta apagada
+			if (_instances[i].activeSelf == false) {
+				_instances [i].SetActive (true);
+				//colocamos la copia en la posicion que recibimos como parametro
+				_instances [i].transform.position = pos;
+				//igual hacemos con la rotacion
+				_instances [i].transform.rotation = rot;
+				//retornamos como resultado la copia
+				return _instances [i];
+
+			}
+		}
+		//si todas las copias estan siendo usadas... retornamos nulo
+		return null;
+	}
+}
---
+++ Taller de Sistemas Touch/Assets/PlayerControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	//esta es la bala que vamos a crear
+	public GameObject _bulletPrefab;
+	//este es el pool de donde vamos a sacar las balas
+	private ObjectPool _bulletPool;
+	private bool shoot;
+	// Use this for initialization
+	void Start () {
+		//esto hace que cada 0.1 segundos se ejecute
+		//la funcion CreateBullet
+		InvokeRepeating ("CreateBullet", 0, 0.1f);
+		//buscamos el pool de las balas. Formamos el nombre juntando el nombre de la bala con la palabra "pool"
+		_bulletPool = GameObject.Find (_bulletPrefab.name + " pool").GetComponent<ObjectPool> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//detectamos cuando la barra espaciadora 
+		//esta siendo presionada
+		if (Input.GetKey (KeyCode.Space)) {
+			shoot = true;
+		} else {
+			shoot = false;
+		}
+	}
+
+	//esta funcion crea una bala
+	void CreateBullet(){
+		if (shoot) {
+			_bulletPool.Spawn (transform.position, transform.rotation);	
+		}
+	}
+}
