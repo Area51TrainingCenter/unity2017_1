4ad422f296e3366f55f8ff5ab709726b6ecadff1
paolo1891 <j18.14p.91m@gmail.com>
2017-09-05 21:56:51 -0500

    Juego taller

---
+++ Platformer Multiplayer Online/ProyectoTallerPaolo/PaoloTaller/Assets/ObjectPool.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour {
+	public GameObject[] _instances;
+	//prefab original d la bala que vamos a clonar
+	public GameObject _prefab;
+	// Use this for initialization
+	void Start () {
+		gameObject.name=_prefab.name+" pool";
+		_instances = new GameObject[25];
+		for (int i = 0; i < 25; i++) {
+			//creamos la bala
+			GameObject nuevaBala=(GameObject)Instantiate(_prefab);
+			nuevaBala.SetActive(false);
+			_instances[i]=nuevaBala;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//esta funcion se encarga de que la bala aparezca
+	public GameObject Spawn (Vector3 pos, Quaternion rot) {
+		for (int i = 0; i < 25; i++) {
+			if (!_instances[i].activeSelf) {
+				_instances [i].SetActive (true);
+				_instances [i].transform.position = pos;
+				_instances [i].transform.rotation = rot;
+				return _instances [i];
+			}
+		}
+		return null;
+	}
+}
---
+++ Platformer Multiplayer Online/ProyectoTallerPaolo/PaoloTaller/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public GameObject _bulletPrebab;
+	public ObjectPool _bulletPool;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("CreateBullet",0,0.1f);
+		Debug.Log("mi archivo es: "+(_bulletPrebab.name+" pool"));
+		_bulletPool=GameObject.Find (_bulletPrebab.name+" pool").GetComponent<ObjectPool>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//detectamos la barra espaciadora
+		if (Input.GetKey(KeyCode.Space)) {
+			
+		}
+	}
+
+	void CreateBullet () {
+		_bulletPool.Spawn (transform.position,transform.rotation);
+		//Instantiate (_bulletPrebab, transform.position, transform.rotation);
+	}
+}
